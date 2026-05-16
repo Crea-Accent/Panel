af0af6a8b578c93e7c125da7f31f69914a3dd12d
Ray De Crom <dummi@macbookdummi.home>
2026-05-16 14:13:34 +0200

    update

--- update.bat
+++ update.bat
@@ -4,11 +4,11 @@ git pull
 
 echo [2/4] Syncing package dependencies...
 :: Clear npm cache to get rid of that "Cannot read properties of null" bug
-call npm cache clean --force 
-call npm install
+call pnpm cache clean --force 
+call pnpm install
 
 echo [3/4] Compiling fresh production build...
-call npm run build
+call pnpm run build
 
 echo [4/4] Bouncing Windows Service...
 :: 🔑 The Secret Sauce: Starts a new, completely separate CMD process to restart the service,
